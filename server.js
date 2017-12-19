805d956d531fe2aa33b7e1337479dccf268df4a8
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2017-12-19 17:27:46 -0600

    i no get it

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+app.post("/api/new", function(req, res) {
+    var newUser = req.body;
+    console.log(newUser);
+    
+})
