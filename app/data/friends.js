266b6e5b4f4f66f658a85afeca6a39125259a583
Jason Saenz <jasonsaenz@Jasons-MacBook-Pro.local>
2018-01-08 21:50:09 -0600

    Hmmm

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,3 +18,5 @@ var users = [
         ]
     }
 ];
+
+module.exports = users;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,12 @@
 /*jshint esversion: 6 */
-
 var path = require("path");
-
 var friendsList = require("../data.friends.js");
 
+module.exports = function(app) {
+  app.get("api/friends", function(req, res) {
+    res.json(friendsList);
+  });
+  app.post("api/friends", function(req, res) {
+    friendsList.push(req.body);
+  });
+};
